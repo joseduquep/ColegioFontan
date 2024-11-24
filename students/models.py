b9f916bbe1d5c6b3d1fb04fcce10263692cbeca3
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-11-24 17:10:13 -0500

    Aplicaciones creadas mas no conectadas

---
+++ students/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)  # Campo ID explícito y autoincrementado
+    name = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    id_number = models.IntegerField(unique=True)
+    autonomy_level = models.CharField(
+        max_length=50,
+        choices=[
+            ('1', 'Nivel 1'),
+            ('2', 'Nivel 2'),
+            ('3', 'Nivel 3'),
+        ],
+    )
+    extended_vacation = models.BooleanField(default=False)
+    grade = models.CharField(
+        max_length=20,
+        choices=[
+            ('primero', 'Primero'),
+            ('segundo', 'Segundo'),
+            ('tercero', 'Tercero'),
+            ('cuarto', 'Cuarto'),
+            ('quinto', 'Quinto'),
+            ('sexto', 'Sexto'),
+            ('septimo', 'Séptimo'),
+            ('octavo', 'Octavo'),
+            ('noveno', 'Noveno'),
+            ('decimo', 'Décimo'),
+            ('once', 'Once'),
+        ],
+    )
+
+    def __str__(self):
+        return f"{self.name} {self.lastname} (ID: {self.id_number})"
