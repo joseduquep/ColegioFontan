e87ff3c22cc98d28e8f5648949157c46a4cec66e
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-11-24 16:10:01 -0500

    Index mejorado con botones funcionales para login y signup

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+#/accounts/...
+
+urlpatterns = [
+    path('signup', views.signup, name='accounts.signup'),
+    path('login/', views.login, name='accounts.login'),
+    path('logout/', views.logout, name='accounts.logout'),
+]
