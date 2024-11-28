18f7178ad1017938fda21e656285124acc62b623
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-11-27 21:01:11 -0500

    Se implemento mostrar todos los estudiantes en el menu principal con redireccion a su horario

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,8 @@ from django import forms
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
 from django.contrib.auth.models import User  # Usamos el modelo User de Django
+from tutors.models import Tutor  # Asegúrate de importar el modelo Tutor
 
-# Personalizamos el formulario de creación de usuario
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(
         required=True,
@@ -21,10 +21,22 @@ class CustomUserCreationForm(UserCreationForm):
         widget=forms.RadioSelect,  # Esto lo convierte en un input tipo radio
         label="¿Eres Tutor?"
     )
+    photo = forms.ImageField(
+        required=False,  # No es obligatorio por defecto
+        widget=forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        help_text="Por favor sube una foto de perfil (solo si eres tutor)."
+    )
 
     def __init__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        # Eliminamos los textos de ayuda por defecto
+        super().__init__(*args, **kwargs)
+        
+        # Hacer el campo `photo` obligatorio solo si es tutor
+        if self.initial.get('is_tutor') == 'yes':
+            self.fields['photo'].required = True
+        else:
+            self.fields['photo'].required = False
+
+        # Actualizamos los estilos del formulario
         for fieldname in ['username', 'password1', 'password2', 'email', 'cedula']:
             self.fields[fieldname].help_text = None
             self.fields[fieldname].widget.attrs.update({
@@ -34,6 +46,11 @@ class CustomUserCreationForm(UserCreationForm):
     def clean(self):
         cleaned_data = super().clean()
         is_tutor = cleaned_data.get('is_tutor')
+        photo = cleaned_data.get('photo')
+
+        # Si el usuario es tutor, asegurarse de que se haya subido una foto
+        if is_tutor == 'yes' and not photo:
+            self.add_error('photo', 'La foto es obligatoria si eres tutor.')
 
         # Si el usuario es tutor, activamos is_staff
         if is_tutor == 'yes':
@@ -42,10 +59,30 @@ class CustomUserCreationForm(UserCreationForm):
             self.instance.is_staff = False
 
         return cleaned_data
-    
+
+    def save(self, commit=True):
+        """
+        Guarda el formulario. Si el usuario es un tutor, también guarda
+        la foto en el modelo Tutor.
+        """
+        user = super().save(commit=False)
+
+        # Guardamos el usuario primero
+        if commit:
+            user.save()
+
+        # Si es un tutor, guardamos la foto en el modelo Tutor
+        if self.cleaned_data.get('is_tutor') == 'yes':
+            tutor = Tutor(user=user)
+            tutor.photo = self.cleaned_data.get('photo')
+            tutor.save()
+
+        return user
+
     class Meta:
         model = User  # Usamos el modelo User de Django
         fields = ['username', 'email', 'cedula', 'password1', 'password2']
+
 
 class CustomErrorList(ErrorList):
     def __str__(self):
--- students/views.py
+++ students/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from . import models
+from .models import Student
 
 def main_menu(request):
-    
-    #query = not query
-    #estudiantes = Estudiante.objects.all()
-    return render(request, 'students/main_menu.html')
+    query = request.GET.get('q', '')  # Obtenemos la consulta de búsqueda
+    if query:
+        students = Student.objects.filter(nombre__icontains=query)  # Filtramos por nombre (ajustar si es necesario)
+    else:
+        students = Student.objects.all()
 
-
-#def search(request):
-    query = request.GET.get('query')
-    #estudiantes = Estudiante.objects.filter(nombre__icontains=query)
-    return render(request, 'estudiantes/home.html', {'estudiantes': estudiantes, 'query': query})
-# Create your views here.
+    return render(request, 'students/main_menu.html', {'students': students, 'query': query})
