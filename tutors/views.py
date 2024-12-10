f4cd602e6fba01800060f4f549accad141dd2bf8
joseduquep <duquepiedrahitaalejandro@gmail.com>
2024-12-10 13:26:46 -0500

    Buscador de la navbar para TUTORES funcional con filtros avanzados

--- tutors/views.py
+++ tutors/views.py
@@ -1,13 +1,21 @@
+# views.py
+
 from django.shortcuts import render, get_object_or_404
 from workshops.models import Workshop
 from .models import Tutor
+from django.db.models import Q  # Importar Q para consultas complejas
 
 # Vista para mostrar los tutores y los workshops
 def show_tutors(request):
     workshops = Workshop.objects.all()  # Obtener todos los workshops
-    query = request.GET.get('q', '')  # Obtener la consulta de búsqueda
+    query = request.GET.get('query', '')  # Obtener la consulta de búsqueda
     if query:
-        tutors = Tutor.objects.filter(nombre__icontains=query)  # Filtrar por nombre
+        # Filtrar por username, first_name o last_name
+        tutors = Tutor.objects.filter(
+            Q(user__username__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+        )
     else:
         tutors = Tutor.objects.all()  # Obtener todos los tutores
     return render(request, 'tutors/show_tutors.html', {'workshops': workshops, 'tutors': tutors, 'query': query})
