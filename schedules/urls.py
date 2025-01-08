6442443ac11b53f4450cdddaec89bdacbe4e62ce
joseduquep <duquepiedrahitaalejandro@gmail.com>
2025-01-08 17:28:27 -0500

    Funcional los titulos de las plantillas hasta el momento

--- schedules/urls.py
+++ schedules/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
     path('select_workshop/<int:student_id>/<str:day>/<int:block_number>/', views.select_workshop, name='select_workshop'),
     path('<int:student_id>/schedule/', views.student_schedule, name='student_schedule'),
