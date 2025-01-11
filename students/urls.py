d781307b5b730c42018c2a5168e2a0e696091031
joseduquep <duquepiedrahitaalejandro@gmail.com>
2025-01-11 15:56:59 -0500

    mas cambios en la miga de pan, y comienzo de arreglo de boton taller base en estudiantes

--- students/urls.py
+++ students/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
     path('student_list/', views.student_list, name='students.student_list'),
     path('register/', views.register_student, name='students.register_student'),
