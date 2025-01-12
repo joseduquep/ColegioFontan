2c89c392f766c130e156065600636f13d27457c8
joseduquep <duquepiedrahitaalejandro@gmail.com>
2025-01-11 22:05:58 -0500

    CRUD de estudiantes terminado

--- students/urls.py
+++ students/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('student_list/', views.student_list, name='students.student_list'),
     path('register/', views.register_student, name='students.register_student'),
-    path('modify/<int:student_id>', views.modify_student, name='students.modify_student'),
+    path('modify/<int:student_id>/', views.modify_student, name='students.modify_student'),
+    path('delete/<int:student_id>/', views.delete_student, name='students.delete_student'),
+    path('delete/confirm/<int:student_id>/', views.confirm_delete_student, name='students.confirm_delete_student'),
+
 
 ]
