682dbe46fec4e7b5944543538fd4fde93bb8894f
joseduquep <duquepiedrahitaalejandro@gmail.com>
2025-01-23 23:30:54 +0000

    permitiendo todos los hosts

--- horariosfontanproyecto/settings.py
+++ horariosfontanproyecto/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oqd^oz@mj@5s!1!jw&xqj!g-ctw@g$0m&g1_9fq5xx__9tn482'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",'horariosistemafontan.online', 'www.horariosistemafontan.online', '45.56.115.110']
+
 
 
 # Application definitionINSTALLED_APPS += ['django_celery_beat']
