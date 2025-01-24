f130d10b7a06f75677cfdf035357b664064ceace
Jose Alejandro Duque <111206283+joseduquep@users.noreply.github.com>
2025-01-24 10:56:16 -0500

    DEBUG == FALSE settings.py

--- horariosfontanproyecto/settings.py
+++ horariosfontanproyecto/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oqd^oz@mj@5s!1!jw&xqj!g-ctw@g$0m&g1_9fq5xx__9tn482'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
